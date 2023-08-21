581279344d15ec5c921816d1e81c0d39ce97e2e8
Igor Montagner <igordsm@gmail.com>
2023-08-21 15:50:36 -0300

    Deployed 10d69b3 with MkDocs version: 1.4.2

--- js/external.js
+++
@@ -1,6 +0,0 @@
-'use-strict'
-
-import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-terminal.mjs");
-import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-button.mjs");
-import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-diagram.mjs");
-import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-external-content.mjs");
