a27d42426f17742b86f3f7a58c45518388fc0179
Igor Montagner <igordsm@gmail.com>
2024-02-14 16:14:39 -0300

    Deployed 3b96169 with MkDocs version: 1.5.3

--- js/external.js
+++ js/external.js
@@ -4,3 +4,5 @@ import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-te
 import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-button.mjs");
 import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-diagram.mjs");
 import("https://cdn.jsdelivr.net/gh/active-handout/web-components@main/src/ah-external-content.mjs");
+
+import("https://unpkg.com/graphviz-webcomponent@2.0.0/dist/graph-bundled.min.js");
